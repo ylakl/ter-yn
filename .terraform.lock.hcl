7b119a72e9482495d07897f4ab5f60cbb523f698
root <root@compute-vm-2-2-20-ssd-1741186332905.ru-central1.internal>
2025-03-14 19:51:45 +0000

    new1

---
+++ .terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version     = "0.139.0"
+  constraints = "0.139.0"
+  hashes = [
+    "h1:gCnpKwb56b+nOvgNcYbD5q5PF6EQTba16UpPu/vDH6A=",
+  ]
+}
---
+++ config.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.139.0"
+    }
+  }
+}
+
+provider "yandex" {
+   token              = "y0__xDl0-yUBBjB3RMgrp3gxRLzqkdKSpIfDjzrCHk9VJmVxCCScQ"
+   cloud_id           = "b1guukrlolhh89d8t696"
+   folder_id          = "b1gctg2bhvuagrakii2l"
+   zone               = "ru-central1-d"
+}
+resource "yandex_compute_instance" "instance-based-on-coi" {
+  name        = "test2"
+  platform_id = "standard-v3"
+
+  resources {
+    cores         = 2
+    memory        = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" //ubuntu-2004-lts
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = "fl87t36qv5u0h81cntot"
+    nat       = true
+  }
+  metadata = {
+   docker-container-declaration = file("${path.module}/declaration.yaml")
+   user-data = file("${path.module}/cloud_config.yaml")
+  }
+}
