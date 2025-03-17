696beb173a637e7039d5a52d7e245494289681df
root <root@compute-vm-2-2-20-ssd-1741186332905.ru-central1.internal>
2025-03-17 19:41:17 +0000

    11

--- config.tf
+++ config.tf
@@ -1,25 +1,25 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "0.139.0"
     }
   }
 }
 
 provider "yandex" {
-   token              = "y0__xDl0-yUBBjB3RMgrp3gxRLzqkdKSpIfDjzrCHk9VJmVxCCScQ"
-   cloud_id           = "b1guukrlolhh89d8t696"
-   folder_id          = "b1gctg2bhvuagrakii2l"
-   zone               = "ru-central1-d"
+  token     = "t1.9euelZqPyZiRmpuclMzImsvKxoqUzO3rnpWazJeRms-Mx8nIiYmNycnHlY7l8_c7CR9B-e84XmJL_d3z93s3HEH57zheYkv9zef1656VmpLKy5mXxsyZis2cxpXKkpyJ7_zF656VmpLKy5mXxsyZis2cxpXKkpyJ.LC_uRUxwki7m_IYGAuYG4gHxu3Et83pI0RaQM40mtekF5M6_u54v0Ior56ZjKVwgY2-aTZnp61W9zsTfOWjKDg"
+  cloud_id  = "b1guukrlolhh89d8t696"
+  folder_id = "b1gctg2bhvuagrakii2l"
+  zone      = "ru-central1-d"
 }
-resource "yandex_compute_instance" "instance-based-on-coi" {
-  name        = "test2"
+resource "yandex_compute_instance" "vm" {
+  name = "terraform1"
   platform_id = "standard-v3"
 
   resources {
-    cores         = 2
-    memory        = 2
+    cores  = 2
+    memory = 2
   }
 
   boot_disk {
@@ -28,13 +28,13 @@ resource "yandex_compute_instance" "instance-based-on-coi" {
       size     = 10
     }
   }
-
   network_interface {
-    subnet_id = "fl87t36qv5u0h81cntot"
-    nat       = true
+    subnet_id = "fl896vf7uql0nbq510ll"
+    nat = true
   }
+ 
   metadata = {
-   docker-container-declaration = file("${path.module}/declaration.yaml")
-   user-data = file("${path.module}/cloud_config.yaml")
+    docker-container-declaration = file("${path.module}/declaration.yaml")
+    user-data = file("${path.module}/cloud_config.yaml")
   }
 }
